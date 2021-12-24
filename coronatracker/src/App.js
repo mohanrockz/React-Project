d214e67209671551b78c08fb8b2e176cd92c0f24
mohanrockz <mohanpujari1999@gmail.com>
2021-12-24 09:17:46 +0530

    data fetched

---
+++ coronatracker/src/App.js
@@ -0,0 +1,15 @@
+import Covid from './components/covid';
+import './App.css';
+import React from 'react';
+
+
+const App = () => {
+  return (
+  <React.Fragment>
+ <Covid/>
+
+ </React.Fragment>
+  );
+}
+
+export default App;
