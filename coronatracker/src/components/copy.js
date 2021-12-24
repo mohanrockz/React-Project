e6c2727b21eb0ad230d1c03072d064804d87f1b2
mohanrockz <mohanpujari1999@gmail.com>
2021-12-24 21:57:44 +0530

    hamburger-menu added

--- coronatracker/src/components/copy.js
+++ coronatracker/src/components/copy.js
@@ -1,4 +1,36 @@
+//------------------------------
+ {/* <section>
+          <marquee text="head"><h1>LIVE TRACKER</h1></marquee>
+           <h1>Corona Tracker</h1>
+          </section>
+<div className='tablayout'>
+<div className='tab1'>
+                <p className='card_name' href="#"><span>DELHI </span></p>
+               <a><p className='card_total card_small1'>Central Delhi</p></a> 
+               <p className='card_total card_small1'>"ACTIVE CASES":{data.active}</p>
+               <p className='card_total card_small1'>"Confirm cases":{data.confirmed}</p>
+               <p className='card_total card_small1'>"Deceased cases":{data.deceased}</p>                 
+        
+        
+</div>
 
+<div className='tab2'>
+    
+</div>
+<div className='tab3'>
+    
+</div>
+
+<div className='tab4'>
+    
+</div>
+
+<div className='tab5'>
+    
+</div>
+ */}
+
+//--------------------------------
 // let selectedOption,  setSelectedOption;
 // function Front() {
 //    const [selectedOption, setSelectedOption] = React.useState("1");
--- coronatracker/src/components/coviddata.js
+++ coronatracker/src/components/coviddata.js
@@ -1,4 +1,8 @@
 import React, { useEffect, useState} from 'react';
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ReactComponent as Logo } from "../logo.svg";
+
 
 import './coviddata.css';
 
@@ -9,10 +13,7 @@ const Coviddata = () =>{
         try{
            var res = await fetch("https://data.covid19india.org/state_district_wise.json");
             const actualdata = await res.json();
-            // var state='Delhi';
-            // console.log(actualdata[`${state}`]);
-            // var allstate='State Unassigned'
-
+    
             console.log(actualdata.Delhi['districtData']['Central Delhi']);
             setData(actualdata.Delhi['districtData']['Central Delhi']);
             } catch(err){
@@ -28,55 +29,89 @@ const Coviddata = () =>{
 
 
     return (
+
         <React.Fragment>
-
-{/* <div style={{
-      backgroundImage:`url(${require("../components/wallpaper1.jpeg").default})`
       
-    }}> */}
-        {/* </div> */}
-        
-        <section>
-          <marquee text="head"><h1>LIVE TRACKER</h1></marquee>
-           <h1>Corona Tracker</h1>
-          </section>
-<div className='tablayout'>
-<div className='tab1'>
-                <p className='card_name' href="#"><span>DELHI </span></p>
-               <a><p className='card_total card_small1'>Central Delhi</p></a> 
-               <p className='card_total card_small1'>"ACTIVE CASES":{data.active}</p>
-               <p className='card_total card_small1'>"Confirm cases":{data.confirmed}</p>
-               <p className='card_total card_small1'>"Deceased cases":{data.deceased}</p>                 
-        
-        
-</div>
+             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="#home">
+         
 
-<div className='tab2'>
-    
-</div>
-<div className='tab3'>
-    
-</div>
-
-<div className='tab4'>
-    
-</div>
-
-<div className='tab5'>
-    
-</div>
-
-
-
-
-
-</div>
-
-
-
-
-
+        <Logo
+          alt=""
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />
+        CORONA TRACKER
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#features">INDIA</Nav.Link>
+          <NavDropdown title="union territories" className='drop1' id="collasible-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1"> Andaman and Nicobar Islands</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2"> Dadra and Nagar Haveli and Daman and Diu</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1"> Chandigarh</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2"> Lakshadweep</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Puducherry</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Ladakh</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Delhi</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Jammu and Kashmir</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Select States" className='drop2' id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Andhra Pradesh</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Arunachal Pradesh</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Assam</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Bihar</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Chandigarh</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Chhattisgarh</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Delhi</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Goa</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Gujarat</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Himachal Pradesh</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Haryana</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Jharkhand</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1"></NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Jammu and Kashmir</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Karnataka</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Kerala</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Maharashtra</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Meghalaya</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Manipur</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Madhya Pradesh</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Mizoram</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Nagaland</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Odisha</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Punjab</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Rajasthan</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Sikkim</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Telangana</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Tamil Nadu</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Tripura</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Uttar Pradesh</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Uttarakhand</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">West Bengal</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            {/* <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item> */}
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#deets">LIVE</Nav.Link>
+          <Nav.Link eventKey={2} href="#memes">
+            SELECT COUNTRY
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
        
+
+
+
+
+
         </React.Fragment>
     )
 }
